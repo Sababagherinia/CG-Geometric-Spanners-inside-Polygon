e1e884143fb775441cc101362c2502c8f38e4d08
Damjan Filipovic <dakifile03@gmail.com>
2025-10-25 16:05:53 +0200

    [classes] Added classes for Points, Segments, Graphs and Polygons - will be progressively updated

---
+++ src/classes.ts
@@ -0,0 +1,49 @@
+/// <reference types="p5/global" />
+class Point {
+  x: number;
+  y: number;
+  
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Segment {
+    src: Point;
+    dest: Point;
+    
+    constructor(src: Point, dest: Point) {
+        this.src = src;
+        this.dest = dest;
+    }
+
+    onSegment(p: Point): Boolean {
+        return false;
+    }
+}
+
+class Graph {
+    points: Point[];
+    edges: Segment[];
+
+    constructor(points: Point[], edges: Segment[]) {
+        this.points = points;
+        this.edges = edges;
+    }
+}
+
+class Polygon {
+    points: Point[];
+    segments: Segment[] = [];
+
+    constructor(points: Point[]) {
+        this.points = points;
+
+        for (let i = 0; i < points.length; i++) {
+            let src = points[i % points.length];
+            let dest = points[(i+1) % points.length];
+            this.segments.push(new Segment(src, dest));
+        }
+    }
+}
