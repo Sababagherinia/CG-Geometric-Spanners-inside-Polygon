072fb5f6469c64d21df0f4006d71c7df2f5ae96b
Damjan Filipovic <dakifile03@gmail.com>
2025-11-25 20:44:47 +0100

    [vertical_segment] Handled the case when the root triangle branches of with two children

--- src/vertical_segment.ts
+++ src/vertical_segment.ts
@@ -1,6 +1,6 @@
 import p5, { TRIANGLES } from "p5";
 import { Point, Polygon, Segment, DualNode, DualTree} from "./classes";
-import {wrapAroundSlice, isInsideTriangle, pointEquality, getMin, compareFn, binarySearch, isEqualPoly} from "./utils";
+import {wrapAroundSlice, isInsideTriangle, pointEquality, getMin, compareFn, binarySearch, isEqualPoly, computeDet, getHalfPoint, getIntersectionPoint} from "./utils";
 
 // PREPROCESSING CODE
 
@@ -73,26 +73,93 @@ function getWeight(polygon: Polygon, previous: Polygon | null, dt: DualTree, poi
 
 function computeVerticalLine(triangles: Point[][], points: Point[]): Segment | null {
 
-  let dt: DualTree = new DualTree(triangles);
-  let previous: Polygon | null = null;
-  let pointsCopy: Point[] = points.slice();
-  let prevWeight: number = 0;
-  let totalWeight = 0;
+  let dt: DualTree = new DualTree(triangles); // dual tree representing the triangulation
+  let previous: Polygon | null = null; // previously processed triangle
+  let pointsCopy: Point[] = points.slice(); // copy of the points triangle
+  let prevWeight: number = 0; // weight of the previous polygon
+  let totalWeight = 0; // weight of all the thus-far visited triangles
 
   while(true) {
-    let nhs: Polygon[] = dt.getNext(previous);
-    if (nhs.length === 0) {
+    // get the next triangle in the dual tree
+    let current: Polygon[] = dt.getNext(previous);
+    if (current.length === 0) {
       return null;
     }
 
-    if (nhs.length === 1) {
-      let innerPoints: Point[] = getInnerPoints(nhs[0], pointsCopy);
+    // if there is one child triangle
+    if (current.length === 1) {
+      // get the points inside the triangle
+      let innerPoints: Point[] = getInnerPoints(current[0], pointsCopy);
       let weight: number = innerPoints.length;
       totalWeight += weight;
 
+      let nextOfCurrent = dt.peekNext(current[0], previous);
+      if (nextOfCurrent.length === 2) { // current triangle has two children in the dual tree
+        let next1: Polygon = nextOfCurrent[0];
+        let next2: Polygon = nextOfCurrent[1];
+
+        let next1InnerPoints = getWeight(next1, previous, dt, pointsCopy);
+        let next2InnerPoints = getWeight(next2, previous, dt, pointsCopy);
+
+        // get the segments of the current triangle with the next triangles in the dual tree
+        let segs: Segment[] | null = dt.getNextSegment(current[0]);
+        if (segs === null)
+          return null;
+
+        // return the next segment of current that corresponds to nh1
+        if (next1InnerPoints.length >= points.length/3 && next1InnerPoints.length <= 2/3*points.length) 
+          return segs[0];
+
+        // return the next segment of the current that corresponds to nh2
+        if (next2InnerPoints.length >= points.length/3 && next2InnerPoints.length <= 2/3*points.length)
+          return segs[1];
+
+        // get the common point between the two segments of the next two triangles
+        let commonPoint: Point = !isEqual(segs[0].src, segs[1].src) && !isEqual(segs[0].src, segs[1].dest) ? segs[0].src : segs[0].dest;
+
+        // the root of the dual tree is an intersection point - it must have >1/3 of all the points in the polygon
+        if (current[0] == dt.root) {
+          // find the segment that splits the root triangle such that one of the sub-polygons has 1/3 of all the points 
+          let otherPointOne: Point = isEqual(segs[0].src, commonPoint) ? segs[0].dest : segs[0].src;
+          let otherPointTwo: Point = isEqual(segs[1].src, commonPoint) ? segs[1].dest : segs[1].src;
+          let oppositeSegment: Segment = new Segment(otherPointOne, otherPointTwo);
+
+          // sort the points inside the root radially clockwise with respect to the commonPoint
+          let innerPointsWithVertices: Point[] = innerPoints.concat([otherPointOne,otherPointTwo]);
+          innerPointsWithVertices.sort((p,q) => computeDet(p, q, commonPoint));
+
+          // going clockwise you are making next1 subpolygon fill up to 1/3 of the points
+          if (isEqual(innerPointsWithVertices[0], otherPointOne)) {
+            let toThird: number = points.length/3 - next1InnerPoints.length;
+            let middleX: number = (innerPointsWithVertices[toThird].x + innerPointsWithVertices[toThird+1].x)/2;
+            let middleY: number = (innerPointsWithVertices[toThird].y + innerPointsWithVertices[toThird+1].y)/2;
+            let segment: Segment = new Segment(commonPoint, new Point(middleX,middleY));
+            let intersectionPoint: Point = getIntersectionPoint(oppositeSegment,segment);
+            return new Segment(commonPoint, intersectionPoint);
+          }
+          
+          // determine 
+        }
+
+        // Condition 3 - Triangle Splits the Polygon into 3 sub-polygons of weight < 1/3
+        let totalPointsVisited = totalWeight - prevWeight;
+        let pointsNeeded = points.length/3 - totalPointsVisited;
+
+        let halfPoint: Point = getHalfPoint(segs[1]);
+        let splittingSegment: Segment = new Segment(commonPoint, halfPoint);
+
+        // calculate the direction that the other points need to be in
+        let previousOfPrevious: Polygon[] = dt.getNeighbours(previous, current);
+        let comparisonPoint: Point;
+        if (previousOfPrevious.length === 0) {
+
+        }
+
+      }
+
       // Condition 1 - Valid Diagonal Case
       if (totalWeight >= points.length/3) {
-        let segs: Segment[] | null = dt.getNextSegment(nhs[0]);
+        let segs: Segment[] | null = dt.getNextSegment(current[0]);
 
         // you only have one neighbour
         if (segs !== null && segs.length === 1)
@@ -105,37 +172,8 @@ function computeVerticalLine(triangles: Point[][], points: Point[]): Segment | n
         return new Segment(new Point(x,-999), new Point(x,999));
       }
 
-      previous = nhs[0];
+      previous = current[0];
       continue;
-    }
-
-    // the triangle is at a point of intersection
-    if (nhs.length === 2) {
-      let nh1: Polygon = nhs[0];
-      let nh2: Polygon = nhs[1];
-
-      let nh1InnerPoints = getWeight(nh1, previous, dt, pointsCopy);
-      let nh2InnerPoints = getWeight(nh2, previous, dt, pointsCopy);
-
-      if (previous === null)
-        return null;
-
-      let segs: Segment[] | null = dt.getNextSegment(previous);
-      if (segs === null)
-        return null;
-
-      // return the next segment of current that corresponds to nh1
-      if (nh1InnerPoints.length >= points.length/3) 
-        return segs[0];
-
-      // return the next segment of the current that corresponds to nh2
-      if (nh2InnerPoints.length >= points.length/3)
-        return segs[1];
-
-      // Condition 3 - Triangle Splits the Polygon into 3 sub-polygons of weight < 1/3
-      let totalPointsVisited = totalWeight - prevWeight;
-      let pointsNeeded = points.length/3 - totalPointsVisited;
-      let commonPoint = !isEqual(segs[0].src, segs[1].src) && !isEqual(segs[0].src, segs[1].dest) ? segs[0].src : segs[0].dest;
     }
   }
 }
